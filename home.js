59e80ac77044d1eecf2b135cb2654bb2435251b5
Maha Sikandar <su92-bscsm-s24-123@superior.edu.pk>
2026-07-23 21:41:14 +0500

    made more changes

--- home.js
+++ home.js
@@ -1,1 +1,33 @@
-c
+const services = document.querySelectorAll(".service");
+
+services.forEach(service => {
+
+    const img = service.querySelector(".service-btn");
+
+    img.addEventListener("click", () => {
+
+        service.classList.toggle("active");
+
+        if(service.classList.contains("active")){
+            img.src = "down.svg";
+        }
+        else{
+            img.src = "next.svg";
+        }
+
+    });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+hiddenElements.forEach(el => observer.observe(el));
